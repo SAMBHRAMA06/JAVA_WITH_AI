d9248d507045b3f6c94c0d33ad2ad7e76af24462
SAMBHRAMA06 <subhaaga10@gmail.com>
2026-05-19 12:26:59 +0530

    Third commit

---
+++ LOGIN_APP/src/main/java/com/login/LoginServlet.java
@@ -0,0 +1,48 @@
+package com.login;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.sendRedirect("login.html");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String userName = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        response.setContentType("text/html");
+
+        PrintWriter pw = response.getWriter();
+
+        if ("admin".equals(userName) && "1234".equals(password)) {
+
+            pw.println("<html><body>");
+            pw.println("<h2>Welcome, " + userName + "!</h2>");
+            pw.println("</body></html>");
+
+        } else {
+
+            pw.println("<html><body>");
+            pw.println("<h2>Invalid Credentials!</h2>");
+            pw.println("</body></html>");
+        }
+    }
+}
